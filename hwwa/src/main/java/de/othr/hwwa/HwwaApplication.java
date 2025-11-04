1acc9f84e4d34cb8b146e501265efcfe8cae0971
martin-coding <martinkoehn.coding@gmail.com>
2025-11-04 20:04:12 +0100

    feat: add springboot template

---
+++ hwwa/src/main/java/de/othr/hwwa/HwwaApplication.java
@@ -0,0 +1,13 @@
+package de.othr.hwwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwwaApplication.class, args);
+	}
+
+}
