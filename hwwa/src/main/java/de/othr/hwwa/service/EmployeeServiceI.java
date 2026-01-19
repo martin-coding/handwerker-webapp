095f43e2822da7e645cbb2fcad68b4a0a8e946ee
martin-coding <89018576+martin-coding@users.noreply.github.com>
2026-01-19 19:51:00 +0100

    feat: add REST API to retrieve employees

--- hwwa/src/main/java/de/othr/hwwa/service/EmployeeServiceI.java
+++ hwwa/src/main/java/de/othr/hwwa/service/EmployeeServiceI.java
@@ -5,11 +5,12 @@ import de.othr.hwwa.model.Role;
 import de.othr.hwwa.model.User;
 import de.othr.hwwa.model.dto.UserDto;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface EmployeeServiceI {
-    public Page<User> getEmployeeList(int page, int size, String sort, String dir, String keyword);
+    public Page<User> getEmployeeList(Pageable pageable, String keyword);
     public UserDto getUserById(long id);
     public List<Role> findAllRoles();
     public void updateEmployee(long id, UserDto dto);
